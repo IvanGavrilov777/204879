4ad69451a22e64b92fa86a54ac30031e4c84da81
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2025-12-19 18:04:28 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "example1" {
+  name     = "ivexample1"
+  location = "West Europe"
+}
+
+/*
+resource "azurerm_resource_group" "example2" {
+  name     = "ivexample-import"
+  location = "West Europe"
+}
+
+*/
