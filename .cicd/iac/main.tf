a7ed7bc13b292f5fac6af71660b0ca8e324f85d6
IvanGavrilov777 <125308112+IvanGavrilov777@users.noreply.github.com>
2025-12-19 18:15:29 +0200

    Update main.tf

--- .cicd/iac/main.tf
+++ .cicd/iac/main.tf
@@ -1,3 +1,4 @@
+/*
 resource "azurerm_resource_group" "example1" {
   name     = "ivexample1"
   location = "West Europe"
@@ -7,10 +8,12 @@ provider "azurerm" {
 features {}
 }
 
-/*
+
 resource "azurerm_resource_group" "example2" {
   name     = "ivexample-import"
   location = "West Europe"
 }
 
 */
+
+resource "null_resource" "ivan" {}
